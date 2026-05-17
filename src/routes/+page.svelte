e30990e60482c3ea276a6cbaa529c59ee03d63b5
Pavel Pasaz <pasazaharenko940@gmail.com>
2026-05-17 16:11:11 +0300

    perf: dynamic import KefineWorkspace on home + skeleton to cut 333KiB unused

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,12 @@
 <script lang="ts">
   import './+page.css';
-  import KefineWorkspace from '$lib/components/kefine/KefineWorkspace.svelte';
+  import type { ComponentType } from 'svelte';
+  let Workspace: ComponentType | null = null;
+  import('$lib/components/kefine/KefineWorkspace.svelte').then((m) => (Workspace = m.default));
 </script>
 
-<KefineWorkspace />
+{#if Workspace}
+  <svelte:component this={Workspace} />
+{:else}
+  <div class="kefine-skeleton" style="min-height:100vh" />
+{/if}
