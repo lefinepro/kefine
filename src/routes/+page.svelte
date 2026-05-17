c49b71a3fd11e8ca43b2c4c6c118828c117e7272
Pavel Pasaz <pasazaharenko940@gmail.com>
2026-05-17 12:51:58 +0300

    revert: keep eager KefineWorkspace on landing to avoid CLS regression

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,6 @@
 <script lang="ts">
   import './+page.css';
-  import type { ComponentType } from 'svelte';
-  let Workspace: ComponentType | null = null;
-  import('$lib/components/kefine/KefineWorkspace.svelte').then(m => Workspace = m.default);
+  import KefineWorkspace from '$lib/components/kefine/KefineWorkspace.svelte';
 </script>
 
-{#if Workspace}
-  <svelte:component this={Workspace} />
-{/if}
+<KefineWorkspace />
