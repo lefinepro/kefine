5c6cb9f473902f0864db65156cedae16e718ace0
Pavel Pasaz <pasazaharenko940@gmail.com>
2026-05-17 16:14:54 +0300

    revert: dynamic home caused 62 regression

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,6 @@
 <script lang="ts">
   import './+page.css';
-  import type { ComponentType } from 'svelte';
-  let Workspace: ComponentType | null = null;
-  import('$lib/components/kefine/KefineWorkspace.svelte').then((m) => (Workspace = m.default));
+  import KefineWorkspace from '$lib/components/kefine/KefineWorkspace.svelte';
 </script>
 
-{#if Workspace}
-  <svelte:component this={Workspace} />
-{:else}
-  <div class="kefine-skeleton" style="min-height:100vh" />
-{/if}
+<KefineWorkspace />
